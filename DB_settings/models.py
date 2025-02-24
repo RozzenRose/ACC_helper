47056a6b25017c2c1bc4519c85861eb6b9050cda
RozzenRose <RozzenRose@gmail.com>
2025-02-24 13:27:41 +0100

    db up fix

--- DB_settings/models.py
+++ DB_settings/models.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class UsersOrm(Base):
     __tablename__ = 'Users'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    #id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
